d4c45966800dccbc6186d55ffc0362191bcbf13c
Jing Xiao <notajingoist@gmail.com>
2014-01-31 23:39:06 -0500

    objectdetect files

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -5,32 +5,6 @@ var SITE = {
 		//this.$el = $('.el');
 
 		this.bindEvents();
-
-
-		// var videoCamera = new tracking.VideoCamera().render();
-		// videoCamera.track({
-		//     type: 'color',
-		//     color: 'magenta',
-		//     onFound: function(track) {
-		//       console.log(track.x, track.y, track.z);
-		//     },
-		//     onNotFound: function() {}
-		// });
-
-		var videoCamera = new tracking.VideoCamera().hide().render().renderVideoCanvas(),
-		        ctx = videoCamera.canvas.context;
-
-		    videoCamera.track({
-		        type: 'human',
-		        data: 'eye',
-		        onFound: function(track) {
-		            for (var i = 0, len = track.length; i < len; i++) {
-		                var rect = track[i];
-		                ctx.strokeStyle = "rgb(0,255,0)";
-		                ctx.strokeRect(rect.x, rect.y, rect.size, rect.size);
-		            }
-		        }
-		    });
 	},
 
 	bindEvents: function() {
