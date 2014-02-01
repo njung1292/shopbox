33edd6ce6c1f9ff34ce82f59b0181d7fb38ba581
Jing Xiao <notajingoist@gmail.com>
2014-02-01 11:38:23 -0500

    David: refactored shopscripts

--- js/src/shopscripts.js
+++ js/src/shopscripts.js
@@ -27,20 +27,19 @@ var SHOP = {
 	},
 
 	initVars: function() {
-		this.tomatoURL = "http://www.ishs.org/sites/default/files/news-images/tomato.jpg";
-		this.carrotURL = "http://plus.maths.org/content/sites/plus.maths.org/files/articles/2011/paraconsistency/carrot.jpg";
-		this.broccoliURL = "http://publishingperspectives.com/wp-content/uploads/2012/03/broccoli.jpg";
-		this.burritoURL = "http://www.tacobell.com/tb_files/cbm/images/burrito-burrito.png";
+		this.photos = {
+			tomato: "http://www.ishs.org/sites/default/files/news-images/tomato.jpg",
+			carrot: "http://plus.maths.org/content/sites/plus.maths.org/files/articles/2011/paraconsistency/carrot.jpg",
+			broccoli: "http://publishingperspectives.com/wp-content/uploads/2012/03/broccoli.jpg",
+			burrito: "http://www.tacobell.com/tb_files/cbm/images/burrito-burrito.png"
+		};
 
-		// this.tomatoURL = "images/sample.png";
-		// this.carrotURL = "images/sample.png";
-		// this.broccoliURL = "images/sample.png";
-		// this.burritoURL = "images/sample.png";
-
-		this.tomatoIconURL = "images/tomato.svg";
-		this.carrotIconURL = "images/carrot.svg";
-		this.broccoliIconURL = "images/broccoli.svg";
-		this.burritoIconURL = "images/burrito.svg";
+		this.icons = {
+			tomato: "images/tomato.svg",
+			carrot: "images/carrot.svg",
+			broccoli: "images/broccoli.svg",
+			burrito: "images/burrito.svg",
+		};
 
 		this.grabbedLimb = "images/carrot.svg";
 		this.ungrabbedLimb = "images/tomato.svg";
@@ -70,55 +69,49 @@ var SHOP = {
 		this.$shelf.append('<img src="' + _iconURL + '" class="produce ' + _filename +'" id="' + _filename + this.itemCount + '">');
 	},
 
+	getProduce: function (pos) {
+		var cx = 300; // center coords of shelves
+		var cy = 300;
+		var r = 300;
+		var x = pos.x - cx;
+		var y = pos.y - cy;
+		// in bounds
+		if (-r < x && x < r && -r < y && y < r) {
+			return (x > 0 && y > 0) ? 'burrito'
+				: (x > 0 && y < 0) ? 'broccoli'
+				: (x < 0 && y > 0) ? 'carrot'
+				: (x < 0 && y < 0) ? 'tomato'
+				: null;
+		}
+		return null;
+	},
+
 	onGrab: function() {
 		$('.limb').attr('src', this.grabbedLimb);
-
-		var grabbed = false;
-		if (this.handPos.x >= 0 && this.handPos.x <= 5) {
-			// Tomato or carrot
-			if (this.handPos.y >= 0 && this.handPos.y <= 5) {
-				// Tomato
-				this.setCurrentItem('tomato', this.tomatoURL, this.tomatoIconURL, this.handPos);
-				grabbed = true;
-			} else if (this.handPos.y >= 0 && this.handPos.y > 5) {
-				// Carrot!
-				this.setCurrentItem('carrot', this.carrotURL, this.carrotIconURL, this.handPos);
-				grabbed = true;
-			}
-		} else if (this.handPos.x >=0 && this.handPos.x > 5) {
-			// Broccoli or burrito
-			if (this.handPos.y >= 0 && this.handPos.y <= 5) {
-				// Broccoli
-				this.setCurrentItem('broccoli', this.broccoliURL, this.broccoliIconURL, this.handPos);
-				grabbed = true;
-			} else if (this.handPos.y >= 0 && this.handPos.y > 5) {
-				// burrito!
-				this.setCurrentItem('burrito', this.burritoURL, this.burritoIconURL, this.handPos);
-				grabbed = true;
-			}
-		}
-		
+		var produce = this.getProduce(this.handPos);
+		this.setCurrentItem('carrot', this.photos[produce], this.icons[produce], this.handPos);
 	},
 
 	onUngrab: function() {
 		$('.limb').attr('src', this.ungrabbedLimb);
 		// Release
-		if (this.currentItem) {
-			// if (this.handPos.x >= 0 && this.handPos.x <= 5) {
-			// 	if (this.handPos.y >= 0 && this.handPos.y <= 5) {
-					console.log('droppin');
-					this.cart.files.push({
-						url: this.currentItem.url,
-						filename: this.currentItem.filename  + this.itemCount
-					});
-					this.itemCount++;
-					//this.$body.trigger('click');
-					//this.saveToDropbox();
-					//this.$shelf.html('');
-			// 	}
-			// }
+		if (this.currentItem && this.handInCart(this.handPos)) {
+			console.log('droppin');
+			this.cart.files.push({
+				url: this.currentItem.url,
+				filename: this.currentItem.filename  + this.itemCount
+			});
+			this.itemCount++;
 		}
 		this.currentItem = null;
+	},
+
+	handInCart: function(pos) {
+		var x = 600; // top left coords of cart
+		var y = 500;
+		var l = 100; // length,width
+		var w = 100;
+		return pos.x > x && pos.x < x + w && pos.y > y && pos.y < y + w;
 	},
 
 	saveToDropbox: function() {
