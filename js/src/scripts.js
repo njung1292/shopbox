e442a41ce3ce4d8d288f47c0e3dfbeedcf4de357
Jing Xiao <notajingoist@gmail.com>
2014-01-31 22:38:37 -0500

    eye tracking

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -5,10 +5,36 @@ var SITE = {
 		//this.$el = $('.el');
 
 		this.bindEvents();
+
+
+		// var videoCamera = new tracking.VideoCamera().render();
+		// videoCamera.track({
+		//     type: 'color',
+		//     color: 'magenta',
+		//     onFound: function(track) {
+		//       console.log(track.x, track.y, track.z);
+		//     },
+		//     onNotFound: function() {}
+		// });
+
+		var videoCamera = new tracking.VideoCamera().hide().render().renderVideoCanvas(),
+		        ctx = videoCamera.canvas.context;
+
+		    videoCamera.track({
+		        type: 'human',
+		        data: 'eye',
+		        onFound: function(track) {
+		            for (var i = 0, len = track.length; i < len; i++) {
+		                var rect = track[i];
+		                ctx.strokeStyle = "rgb(0,255,0)";
+		                ctx.strokeRect(rect.x, rect.y, rect.size, rect.size);
+		            }
+		        }
+		    });
 	},
 
 	bindEvents: function() {
-		this.$el.on('click', this.functionName.bind(this));
+		//this.$el.on('click', this.functionName.bind(this));
 	},
 
 	functionName: function(e) {
